99737f40265b80c4f22d03842d3f03e87e727532
MakineIUT <makinemhoumadi09@gmail.com>
2026-03-31 18:15:24 +0200

    bug

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,29 +1,34 @@
 using UnityEngine;
 using System.Linq;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float rotationSpeed = 720f; // Degrees per second
     [SerializeField] private float vitesse = 5f;
-    [SerializeField] private float  hauteurSaut= 2f;
+    [SerializeField] private float  hauteurSaut= 1f;
     [SerializeField] private float gravite = -9.8f;
-    [SerializeField] private int playerIndex = 0;
+
     
 
+    private Animator animator;
     private CharacterController controller;
     private Vector2 moveInput;
     private Vector3 velocity;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        GetComponent<PlayerInput>();
+    }
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
     }
 
-    public int GetPlayerIndex()
-    {
-        return playerIndex;
-    } 
+
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -37,17 +42,39 @@ public class PlayerController : MonoBehaviour
         Debug.Log($"Jumping {context.performed} -Is Grounded: {controller.isGrounded}");
         if (context.performed && controller.isGrounded)
         {
-            Debug.Log("Le perso doit sauter");
+            animator.SetTrigger("Jump"); 
             velocity.y = Mathf.Sqrt(hauteurSaut * -2f * gravite);
         }
     }
     // Update is called once per frame
     void Update()
-    {
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        controller.Move(move * vitesse * Time.deltaTime);
+{
+    // 1. Calculate movement direction relative to the world
+    Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+    controller.Move(move * vitesse * Time.deltaTime);
 
-        velocity.y += gravite * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+    // 2. SMOOTH ROTATION LOGIC
+    if (moveInput.sqrMagnitude > 0.01f) // Only rotate if moving
+    {
+        // Calculate the direction we want to look at
+        Quaternion targetRotation = Quaternion.LookRotation(move);
+
+        // Smoothly rotate the player toward that target
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation, 
+            targetRotation, 
+            rotationSpeed * Time.deltaTime
+        );
     }
+
+    // 3. ANIMATION LOGIC
+    if (animator != null)
+    {
+        animator.SetBool("IsRunning", moveInput.magnitude > 0.1f);
+    }
+
+    // 4. GRAVITY LOGIC
+    velocity.y += gravite * Time.deltaTime;
+    controller.Move(velocity * Time.deltaTime);
+}
 }
